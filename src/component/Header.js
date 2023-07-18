97a82844faee06e68a12778a43cb81acc09aab15
MKPourkarim <mkpourkarim@gmail.com>
2023-07-18 10:10:06 +0200

    data fetched with axios

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,15 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 import '../index.css'
 
 const Header = () => {
+    const [data, setData] = useState([])
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res => setData(res.data))
+        .catch(err => console.log(err))
+    },[])
+    
     return(
         <h className='header'>Header</h>
     )
--- src/component/screens/User.js
+++ src/component/screens/User.js
@@ -1,13 +1,45 @@
-import Header from '../Header'
 import '../../index.css'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+
 const User = () => {
+    const [data, setData] = useState([])
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res => setData(res.data))
+        .catch(err => console.log(err))
+    },[])
+    
     return(
         <div>
-            
-            <h1>Blank page</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((user, index)=>{
+                            return (
+                                <tr key={index}>
+                                    <td>{user.id}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.address.city}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
-
 
 export default User;
